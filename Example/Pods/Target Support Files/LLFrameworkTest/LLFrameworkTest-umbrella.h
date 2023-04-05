2dc82662ee830c89b84023e34391918f3cdad966
陈宏伟 <1794437065@qq.com>
2023-04-05 14:00:11 +0800

    Release 0.0.1.

---
+++ Example/Pods/Target Support Files/LLFrameworkTest/LLFrameworkTest-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "LLFrameworkTestContext.h"
+#import "Target_LLFrameworkTest.h"
+
+FOUNDATION_EXPORT double LLFrameworkTestVersionNumber;
+FOUNDATION_EXPORT const unsigned char LLFrameworkTestVersionString[];
+
